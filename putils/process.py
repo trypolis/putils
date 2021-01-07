4b706e7d0e7962ba4f7f563b9a4f6720088e862f
Ty <turret1226@gmail.com>
2021-01-07 08:38:46 -0700

    Re-organized the code.

--- putils/process.py
+++ putils/process.py
@@ -5,23 +5,12 @@
 # MIT Licensed. See the LICENSE file more spacific
 # licencing terms.
 
-import sys
+"""Process killing functions."""
+
 import platform
 import ctypes
 import os
 import signal
-
-
-def log_tracebacks(log_file: str, append: bool = True) -> bool:
-    """Logs any tracebacks to sys.stderr."""
-    if append:
-        temp_file: file = open(log_file, "a")
-    else:
-        temp_file: file = open(log_file, "w")
-    if temp_file is None:
-        return False
-    sys.stderr = temp_file
-    return True
 
 
 def kill_windows_process(pid: int):
