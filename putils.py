fab3c1a03088254332b592c41eb415f4f2a8f0bb
Ty <turret1226@gmail.com>
2021-01-07 08:35:30 -0700

    Added cross-platform process killing.

--- putils.py
+++ putils.py
@@ -6,6 +6,10 @@
 # licencing terms.
 
 import sys
+import platform
+import ctypes
+import os
+import signal
 
 
 def log_tracebacks(log_file: str, append: bool = True) -> bool:
@@ -18,3 +22,33 @@ def log_tracebacks(log_file: str, append: bool = True) -> bool:
         return False
     sys.stderr = temp_file
     return True
+
+
+def kill_windows_process(pid):
+    """Kill a Windows process."""
+    PROCESS_TERMINATE = 1
+    SYNCHRONIZE = 1048576
+    handle = ctypes.windll.kernel32.OpenProcess(
+        PROCESS_TERMINATE | SYNCHRONIZE, False, pid
+    )
+    ctypes.windll.kernel32.TerminateProcess(handle, -1)
+    ctypes.windll.kernel32.WaitForSingleObject(handle, 1000)
+    ctypes.windll.kernel32.CloseHandle(handle)
+
+
+def kill_unix_process(pid):
+    """Kills a process on Unix systems, such as Mac or Linux."""
+    try:
+        os.kill(pid, signal.SIGKILL)
+    except OSError:
+        pass
+
+
+def kill_process(pid):
+    """Forcefully kills a process."""
+    if pid < 0:
+        return
+    if platform.system() == "Windows":
+        kill_windows_process(pid)
+    else:
+        kill_unix_process(pid)
